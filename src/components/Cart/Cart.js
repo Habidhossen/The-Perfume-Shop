43e09cea8564b92cb3a77588875b835ada3fdd6c
Habidhossen <habidhossen2@gmail.com>
2022-03-26 00:55:46 +0600

    responsiveness added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,6 +6,9 @@ const Cart = () => {
     <div className="cart-container">
       <h1 className="cart-title">Selected Perfume:</h1>
       <div>
+        <h5 className="cart-items">Gentleman Reserve Privee</h5>
+      </div>
+      <div>
         <button className="cart-btn-one">CHOOSE 1 FOR ME</button>
         <button className="cart-btn-two">RESET</button>
       </div>
