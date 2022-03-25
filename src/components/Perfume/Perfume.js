09e4da34ca28b85f03240591e88db57d1890d4c2
Habidhossen <habidhossen2@gmail.com>
2022-03-25 22:27:37 +0600

    fetch data

--- src/components/Perfume/Perfume.js
+++ src/components/Perfume/Perfume.js
@@ -1,18 +1,16 @@
 import React from "react";
 import "./Perfume.css";
 
-const Perfume = () => {
+const Perfume = (props) => {
+  const { name, price, image } = props.product;
+
   return (
     <div className="card">
       <div>
-        <img
-          className="card-img"
-          src="https://www.theperfumeshop.com/medias/sys_master/front-zoom/front-zoom/10935595270174/Ralph-Lauren-Eau-de-Toilette-for-him-3605970416003-Polo-Red.jpg"
-          alt=""
-        />
+        <img className="card-img" src={image} alt="" />
       </div>
-      <h1 className="card-title">Sauvage Elixir</h1>
-      <p className="card-price">Price: $99.99</p>
+      <h1 className="card-title">{name}</h1>
+      <p className="card-price">Price: ${price}</p>
       <button className="card-btn">Add to Cart</button>
     </div>
   );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Cart from "../Cart/Cart";
 import Perfume from "../Perfume/Perfume";
 import "./Products.css";
 
 const Products = () => {
+  // set useSate
+  const [products, setProducts] = useState([]);
+
+  // set useEffect
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <div>
-      <h1>The Perfume Shop</h1>
-      <p>Welcome to the Fragrance World!</p>
-
-      <div className="product-container">
-        <Perfume></Perfume>
+      <div>
+        <h1>The Perfume Shop</h1>
+        <p>Welcome to the Fragrance World!</p>
+      </div>
+      <div className="container">
+        <div className="product-container">
+          {products.map((product) => (
+            <Perfume key={product.id} product={product}></Perfume>
+          ))}
+        </div>
         <Cart></Cart>
       </div>
     </div>
