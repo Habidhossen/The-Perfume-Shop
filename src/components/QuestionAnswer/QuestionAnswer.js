543c83bedb510a2f03fc4a404f1e057af0182de4
Habidhossen <habidhossen2@gmail.com>
2022-03-26 17:42:41 +0600

    answer added

--- src/components/QuestionAnswer/QuestionAnswer.js
+++ src/components/QuestionAnswer/QuestionAnswer.js
@@ -9,17 +9,24 @@ const QuestionAnswer = () => {
       <div className="ques-ans">
         <h3>How React Works?</h3>
         <p>
-          <b>Answer: </b>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae,
-          error!
+          <b>Answer: </b>A very popular javascript library for building user
+          interfaces whose name is React. React basically creates a virtual DOM
+          (Document Object Model). When need to read or write browser DOM then
+          virtual DOM represents a virtual representation. Then virtual DOM
+          tries to most efficient and fastest way to update the browser DOM. And
+          thus React works.
         </p>
       </div>
       <div className="ques-ans">
         <h3>Difference between Props vs State.</h3>
         <p>
           <b>Answer: </b>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae,
-          error!
+          In React, the two most fundamental parts are props and state. The main
+          difference between props and stats is Props have passed data from one
+          component to another. It is read-only which means cannot be modified
+          and is immutable. On the other side, State has passed data within the
+          component only. The state is mutable it's can be modified and both
+          read and written.
         </p>
       </div>
     </div>
